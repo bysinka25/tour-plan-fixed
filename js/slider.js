66f4b2db7594defba225ede142cbeb3b49195abe
bysinka25 <kirgizilla@gmail.com>
2021-05-27 16:34:36 +0300

    create modal

--- js/slider.js
+++ js/slider.js
@@ -1,35 +1,61 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  effect: "coverflow",
-  keyboard: {
-    enabled: "true",
-    onlyInViewport: "true",
-  },
-});
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  effect: "slide",
-  keyboard: {
-    enabled: "true",
-    onlyInViewport: "true",
-  },
-});
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    effect: "coverflow",
+    keyboard: {
+      enabled: "true",
+      onlyInViewport: "true",
+    },
+  });
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    effect: "slide",
+    keyboard: {
+      enabled: "true",
+      onlyInViewport: "true",
+    },
+  });
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  $(document).keyup(function (event) {
+    if (event.which == "27") {
+      $(".modal__overlay").removeClass("modal__overlay--visible");
+      $(".modal__dialog").removeClass("modal__dialog--visible");
+    }
+  });
 });
